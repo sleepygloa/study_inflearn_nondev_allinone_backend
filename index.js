65b75a9a693acd1e6c0e6691c110ee3baff87791
sleepygloa <sleepygloa@gmail.com>
2021-03-28 11:57:25 +0900

    결제&반응형 부분 커밋

--- index.js
+++ index.js
@@ -20,12 +20,26 @@ app.use(express.json());
 app.use(cors());
 app.use('/uploads' ,express.static('uploads'));
 
+app.get('/banners', (req,res)=>{
+    models.Banner.findAll({
+        limit:2
+    }).then((result)=>{
+        res.send({
+            banners:result
+        })
+    }).catch((error)=>{
+        console.log(error);
+        res.status(500).send('에러가 발생했습니다.',error);
+    })
+
+})
+
 app.get("/products", (req,res) => {
     // res.send("업로드 된 상품들입니다.");
     //const query = req.query;
     models.Product.findAll({
         order : [['createdAt', 'DESC']],
-        attributes:['id','name','price','createdAt','seller', 'imageUrl']
+        attributes:['id','name','price','createdAt','seller', 'imageUrl', 'soldout', 'type']
     })
     .then((result)=>{
         console.log(result);
@@ -73,7 +87,8 @@ app.post("/products", (req, res) => {
             description,
             price,
             seller,
-            imageUrl
+            imageUrl,
+            type
         }).then((result)=>{
             console.log("상품 생성 결과 : ",result);
             res.send({
@@ -114,6 +129,49 @@ app.post('/image', upload.single('image'), (req, res) =>{
     res.send({
         imageUrl : file.path,
     })
+})
+
+app.post('/purchase/:id', (req, res)=>{
+    const {id} = req.params;
+    models.Product.update({
+        soldout:1
+    },{
+        where: {
+            id
+        }
+    }).then((result)=>{
+        res.send({result:true})
+    }).catch((error)=>{
+        console.error(error);
+        res.status(500).send('에러가 발생했습니다.', error);
+    })
+})
+
+app.get('/products/:id/recommendation', (req, res)=>{
+    const {id} = req.params;
+    models.Products.findOne({
+        where:{
+            id
+        }
+    }).then((product)=>{
+        console.log(product);
+        const type = products.type;
+        models.Product.findAll({
+            where:{
+                type,
+                id : {
+                    [models.Sequelize.Op.ne]:id
+                }
+            }
+        }).then((products)=>{
+            res.send({products});
+        })
+        // res.send(id);
+    }).catch((error)=>{
+        console.error(error);
+        res.status(500).send("에러가 발생 했습니다.", error);
+    })
+    
 })
 
 app.listen(port, () => {
